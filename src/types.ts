890db25c7762420af83076284152d67ec9c1833f
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-11-24 17:24:23 -0500

    Normalize indentation

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,17 @@
 export enum DayOfWeek {
-    Monday = 1,
-    Tuesday = 2,
-    Wednesday = 3,
-    Thursday = 4,
-    Friday = 5,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
 }
 
 export interface CourseEvent {
-    pdfIndex: number;
-    time: string;
-    day: DayOfWeek;
-    title: string;
-    block: string;
-    teacher?: string;
-    room?: string;
-  }
-  
+  pdfIndex: number;
+  time: string;
+  day: DayOfWeek;
+  title: string;
+  block: string;
+  teacher?: string;
+  room?: string;
+}
