f7d46fb93140b5bad29877ea7ce6ec28694808cf
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-11-27 10:45:25 -0500

    Add drag and drop support to PDF Upload

--- src/PdfUpload.svelte
+++ src/PdfUpload.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
-  import parsePdf from "./pdfParser";
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+  import { quadOut } from "svelte/easing";
+
+  const dispatch = createEventDispatcher<{upload: File}>();
   
   let files: FileList;
 
-  const test = async () => {
-    console.log(await parsePdf(files[0]));
+  let dragging = false;
+  let lastDropTarget = null;
+
+  $: if (files) {
+    dispatch("upload", files[0]);
   }
-  $: if (files?.length === 1) test();
+
+  const handleDragEnter = (e: DragEvent) => {
+    if (e.dataTransfer.types.includes("Files")) {
+      lastDropTarget = e.target;
+      dragging = true;
+    }
+  };
+
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDragLeave = (e: DragEvent) => {
+    if (e.target === lastDropTarget || e.target === document) {
+      dragging = false;
+    }
+  };
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    dragging = false;
+    files = e.dataTransfer.files;
+  };
 </script>
   
 <div class="box">
@@ -16,6 +45,19 @@
     Or Browse
   </label>
 </div>
+
+<svelte:window
+  on:dragenter={handleDragEnter}
+  on:dragover={handleDragOver}
+  on:dragleave={handleDragLeave}
+  on:drop={handleDrop}
+/>
+
+{#if dragging}
+  <div class="drop-overlay" transition:fade={{duration: 200, easing: quadOut}}>
+    <h1>Drop anywhere!</h1>
+  </div>
+{/if}
 
 <style>
   .box {
@@ -31,5 +73,19 @@
     border: 1px solid #ccc;
     border-radius: 0.25rem;
     cursor: pointer;
+  }
+
+  .drop-overlay {
+    display: grid;
+    place-content: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    background: var(--accent);
+    color: white;
+    opacity: 80%;
   }
 </style>
