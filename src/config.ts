e6aec6a19c698153dc7e812878681c3f4a03f3c5
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-10-21 23:22:54 -0400

    Update config (default date range)

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { DateTime, Interval } from "luxon";
 
 export const defaultDateRange = Interval.fromDateTimes(
-  DateTime.local(2023, 1, 4, { zone: "America/New_York" }),
-  DateTime.local(2023, 6, 10, { zone: "America/New_York" })
+  DateTime.local(2023, 9, 5, { zone: "America/New_York" }),
+  DateTime.local(2023, 12, 15, { zone: "America/New_York" })
 );
 
 export const minAMHour = 7;
