b3d5f7ea9a1bbe5d92c7f2f48c6efdfce560ebe9
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-09-02 21:51:39 -0400

    Support 2024-2025 pdf schedule format

--- docs/schedule_format.md
+++ docs/schedule_format.md
@@ -1,44 +1,67 @@
-Notes about the format of text extracted (with PDF.js) from schedule PDF files, and steps to parse it.
-
 # The Schedule Format
+Notes about the format of schedule documents.
 
-## Header
+## Per-Block Text Content
 ```
-Student Name, 10
+Course Title
+1:23 - 2:46 (Peroid A)
+Dr. Instructor Name - Location
+```
+```
+Course Title
+1:23 - 2:46 (Peroid A)
+Dr. Instructor Name - None
+```
+```
+Course Title
+1:23 - 2:46 (Peroid A)
+None - Location
+```
 
-Advisor: Dr. Advisor Name, Fall 2022
+## HTML
+```
+body#page > div#container > div#document > div.schedule-block
+    div.day.day_2 // Monday
+        div.block > p
+            ... // Per-block text content
+        ...
+    div.day.day_6 // Friday
+        ...
+    ...
+```
+
+## PDF
+```
+Student Name - Semester 1
+
+Advisor: Dr. Advisor Name
+Grade 10
 
 Monday
-
+ 
 Tuesday
-
+ 
 Wednesday
-
+ 
 Thursday
-
+ 
 Friday
 
-<#SPLIT# ~sdf-s{15173}-a{23329}/>
-```
+8 AM
+9 AM
+10 AM
+11 AM
+12 PM
+1 PM
+2 PM
+3 PM
+4 PM
+5 PM
+6 PM
+7 PM
+Course Title // Per-block text content
+1:23 - 2:46 (Peroid A)
+Dr. Instructor Name - Location
 
-## Per course
+Printed Sep 2, 2024
 ```
-123                             <- Room? (all-caps)
-1:23 - 2:46                     <- Time
-A                               <- Block (all-caps)
-Course Name Goes Here and May   <- Title
-Span Multiple Lines
-                                <- Empty line
-Dr. Instructor Name             <- Teacher?
-```
-
-# How to parse
-* Parse courses breadth-first, parsing each property on every course before moving to the next property.
-* Mark parsed lines as consumed, so that they cannot be parsed again as part of a different course
-1. Every course has a time, so use that as the main anchor point
-    * Times do not specify am/pm, so assume a time range of 6 am - 5:59 pm
-1. The i+1 line is the block
-1. The i+2 line is always the first line of the title
-1. if i-1 in all-caps+numbers & is not consumed, it is room
-1. Continue parsing the title from i+3, until a consumed line is reached or:
-    * If the line starts with "Dr.", "Ms.", "Mr.", "Mrs.", "Mx.", it is teacher
--- src/lib/pdfParser.ts
+++ src/lib/pdfParser.ts
@@ -3,96 +3,75 @@ import type { TextItem } from "pdfjs-dist/types/src/display/api";
 import { minAMHour } from "../config";
 import { DayOfWeek, Time, type CourseEvent } from "./types";
 
-const COLUMNS = [
-  {day: DayOfWeek.Monday, x: 38},
-  {day: DayOfWeek.Tuesday, x: 147},
-  {day: DayOfWeek.Wednesday, x: 255},
-  {day: DayOfWeek.Thursday, x: 363},
-  {day: DayOfWeek.Friday, x: 471},
-].reverse();
+const line2Regex = /^(\d{1,2}):(\d{2}) - (\d{1,2}):(\d{2}) \(Period (.*)\)$/; // 8:00 - 9:30
 
-const timeStringRegex = /^(\d{1,2}):(\d{2}) - (\d{1,2}):(\d{2})$/; // 8:00 - 9:30
-const roomStringRegex = /^[A-Z0-9]+$/; // 205, ASM
+const parsePDF = async (file: File) => {
+  // Cached constants and regexes
+  const COLUMNS = [ // minimum x values for each day
+    {day: DayOfWeek.Monday, x: 48},
+    {day: DayOfWeek.Tuesday, x: 190},
+    {day: DayOfWeek.Wednesday, x: 330},
+    {day: DayOfWeek.Thursday, x: 475},
+    {day: DayOfWeek.Friday, x: 618},
+  ].reverse();
 
-type pdfCourseEvent = CourseEvent & { pdfIndex: number };
-
-const getRelevantContent = async (file: File) => {
+  // Load and extract text content
   const buffer = await file.arrayBuffer();
   const document = await pdfjs.getDocument(buffer).promise;
   const page = await document.getPage(1);
-  const content = await page.getTextContent({
-    includeMarkedContent: false,
-    disableCombineTextItems: false
-  });
-  const items = content.items as TextItem[];
-  return items.slice(
-    content.items.findIndex(
-      (item) => "str" in item && item.str.startsWith("<#SPLIT#")
-    ) + 1
-  )
-}
+  const content = await page.getTextContent();
+  let textItems = content.items as TextItem[];
 
-const parsePDF = async (file: File) => {
-  const pdfContent = await getRelevantContent(file);
-  const consumedItems: boolean[] = [];
-  const courses: pdfCourseEvent[] = [];
+  // Trim and exclude whitespace
+  for (const item of textItems) item.str = item.str.trim();
+  textItems = textItems.filter((item) => item.str);
 
-  // Time, day, block, title
-  for (let i = 0; i < pdfContent.length; i++) {
-    const item = pdfContent[i];
-
-    const timeRegexResult = timeStringRegex.exec(item.str);
-    if (timeRegexResult) {
-      consumedItems[i] = true;
-
-      let startTime = new Time(Number(timeRegexResult[1]), Number(timeRegexResult[2]));
-      if (startTime.hours < minAMHour) startTime = new Time(startTime.hours + 12, startTime.minutes);
-      let endTime = new Time(Number(timeRegexResult[3]), Number(timeRegexResult[4]));
-      if (endTime.hours < minAMHour) endTime = new Time(endTime.hours + 12, endTime.minutes);
-
-      const day = COLUMNS.find(
-        (column) => column.x <= item.transform[4]
-      ).day;
-
-      const block = pdfContent[i+1].str;
-      consumedItems[i+1] = true;
-
-      const title = pdfContent[i+2].str;
-      consumedItems[i+2] = true;
-
-      courses.push({ pdfIndex: i, startTime, endTime, day, block, title });
-    }
+  // Find first line of actual schedule
+  // (first line that doesn't end with "PM" after any lines that do)
+  let start = 0;
+  for (; start < textItems.length; start++) {
+    if (textItems[start].str.endsWith("PM")) break;
+  }
+  for (; start < textItems.length; start++) {
+    if (!textItems[start].str.endsWith("PM")) break;
   }
 
-  // Room
-  for (const courseEvent of courses) {
-    const i = courseEvent.pdfIndex - 1;
-    if (
-      roomStringRegex.test(pdfContent[i].str)
-      && !consumedItems[i]
-    ) {
-      courseEvent.room = pdfContent[i].str;
-      consumedItems[i] = true;
-    }
-  }
+  // Slice to only include schedule
+  textItems = textItems.slice(start, -1);
+  
+  const courses: CourseEvent[] = [];
 
-  // Additional title lines, teacher
-  for (const courseEvent of courses) {
-    for (
-      let i = courseEvent.pdfIndex + 3;
-      i < pdfContent.length && !consumedItems[i];
-      i++
-    ) {
-      const item = pdfContent[i];
-      if (item.str.trim().length === 0) {
-        i++;
-        if (i < pdfContent.length && !consumedItems[i]) {
-          courseEvent.teacher = pdfContent[i].str;
-        }
-        break;
-      };
-      courseEvent.title += " " + item.str;
-    }
+  for (let i = 0; i+2 < textItems.length; i+=3) {
+    const line1 = textItems[i].str;
+    const line2 = textItems[i+1].str;
+    const line3 = textItems[i+2].str;
+
+    const line2Parsed = line2Regex.exec(line2);
+    if (!line2Parsed) throw new Error(`Invalid line "${line2}" for course "${line1}"`);
+
+    let startTime = new Time(Number(line2Parsed[1]), Number(line2Parsed[2]));
+    if (startTime.hours < minAMHour) startTime = new Time(startTime.hours + 12, startTime.minutes);
+    let endTime = new Time(Number(line2Parsed[3]), Number(line2Parsed[4]));
+    if (endTime.hours < minAMHour) endTime = new Time(endTime.hours + 12, endTime.minutes);
+
+    const day = COLUMNS.find(
+      (column) => column.x <= textItems[i].transform[4]
+    )?.day;
+    if (!day) throw new Error(`Invalid day (x=${textItems[i].transform[4]}) for course ${line1}`);
+
+    let line3Split = line3.split("-")
+      .map((str) => str.trim())
+      .map((str) => str === "None" ? undefined : str)
+
+    courses.push({
+      title: line1,
+      startTime,
+      endTime,
+      day,
+      block: line2Parsed[5],
+      teacher: line3Split[0],
+      room: line3Split[1],
+    })
   }
 
   return courses;
