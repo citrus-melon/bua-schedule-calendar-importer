91b84e185612624a9e0997ac475b3f3df2f9408a
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-09-02 21:54:51 -0400

    Bump default date range

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { DateTime, Interval } from "luxon";
 
 export const defaultDateRange = Interval.fromDateTimes(
-  DateTime.local(2024, 1, 3, { zone: "America/New_York" }),
-  DateTime.local(2024, 6, 1, { zone: "America/New_York" })
+  DateTime.local(2024, 9, 3, { zone: "America/New_York" }),
+  DateTime.local(2024, 12, 21, { zone: "America/New_York" })
 );
 
 export const minAMHour = 7;
